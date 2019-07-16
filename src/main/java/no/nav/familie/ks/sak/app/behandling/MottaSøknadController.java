28ed7e0828b58103fb8a6120b7b8acae5a0462c2
Henning Håkonsen <henninghaakonsen@me.com>
2019-07-16 12:06:44 +0200

    Legger til dependency for å få metrikker til å fungere. Setter opp enkel metrikk for telling av automatisk godkjente søknader for testing.

--- src/main/java/no/nav/familie/ks/sak/app/behandling/MottaSøknadController.java
+++ src/main/java/no/nav/familie/ks/sak/app/behandling/MottaSøknadController.java
@@ -1,5 +1,10 @@
 package no.nav.familie.ks.sak.app.behandling;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
+import no.nav.familie.ks.sak.config.ApplicationConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +20,18 @@ import no.nav.security.oidc.api.ProtectedWithClaims;
 @ProtectedWithClaims(issuer = "intern")
 public class MottaSøknadController {
 
+    private static final Logger log = LoggerFactory.getLogger(MottaSøknadController.class);
+
+    private final Counter soknadAutomatiskGodkjent = Metrics.counter("soknad.kontantstotte.automatisk.godkjent");
+
     public MottaSøknadController() {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, path = "dokument")
-    public ResponseEntity mottaDokument(@RequestBody String soeknad) {
+    public ResponseEntity mottaDokument(@RequestBody String soknad) {
+        log.info(soknad);
+
+        soknadAutomatiskGodkjent.increment();
 
         return new ResponseEntity(HttpStatus.OK);
     }
