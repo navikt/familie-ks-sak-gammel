8b4c71af3b1d1d2f66597708c3c0e3e493a52256
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-08-01 12:20:34 +0200

    Rydding

--- src/main/java/no/nav/familie/ks/sak/app/grunnlag/Oppslag.java
+++ src/main/java/no/nav/familie/ks/sak/app/grunnlag/Oppslag.java
@@ -1,6 +1,7 @@
 package no.nav.familie.ks.sak.app.grunnlag;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import no.nav.familie.ks.sak.app.MottaSøknadController;
 import no.nav.familie.ks.sak.app.integrasjon.personopplysning.OppslagException;
 import no.nav.familie.ks.sak.app.integrasjon.personopplysning.domene.PersonhistorikkInfo;
@@ -24,38 +25,34 @@ import java.time.Duration;
 public class Oppslag {
 
     private URI oppslagServiceUri;
-    private ObjectMapper mapper;
     private HttpClient client;
     private static final Logger logger = LoggerFactory.getLogger(MottaSøknadController.class);
+    public static final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     public Oppslag(@Value("${FAMILIE_KS_OPPSLAG_API_URL}") URI oppslagServiceUri) {
         this.oppslagServiceUri = oppslagServiceUri;
-        this.mapper = new ObjectMapper();
         this.client = create();
     }
 
-    public static HttpClient create() {
+    private static HttpClient create() {
         return HttpClient.newBuilder()
                 .connectTimeout(Duration.ofSeconds(10))
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
     }
 
+    private static HttpRequest request(URI uri) {
+        return HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Nav-Call-Id", MDC.get(MDCConstants.MDC_CORRELATION_ID))
+                .GET()
+                .build();
+    }
+
     public TpsFakta hentTpsFakta(Søknad søknad, String personident) {
-
-        String aktørId = hentAktørId(personident);
-
-        Forelder forelder = genererForelder(aktørId);
-
-        var personidentForAnnenForelder = søknad.familieforhold.annenForelderFodselsnummer;
-        Forelder annenForelder = null;
-        if (! personidentForAnnenForelder.isEmpty()) {
-            var aktørIdAnnenForelder = hentAktørId(personidentForAnnenForelder);
-            annenForelder = genererForelder(aktørIdAnnenForelder);
-        }
-
+        Forelder forelder = genererForelder(hentAktørId(personident));
+        Forelder annenForelder = finnAnnenForelder(søknad);
         Personinfo barn = finnBarnSøktFor(søknad, forelder.getPersoninfo());
-
         return new TpsFakta.Builder()
                 .medForelder(forelder)
                 .medBarn(barn)
@@ -70,10 +67,18 @@ public class Oppslag {
                 .build();
     }
 
+    private Forelder finnAnnenForelder(Søknad søknad) {
+        var personident = søknad.familieforhold.annenForelderFodselsnummer;
+        if (! personident.isEmpty()) {
+            var aktørId = hentAktørId(personident);
+            return genererForelder(aktørId);
+        }
+        else return null;
+    }
+
     private Personinfo finnBarnSøktFor(Søknad søknad, Personinfo personinfo) {
         // TODO: Returner fnr for valgt barn i tillegg til fødselsdato
         String personIdentBarn = søknad.mineBarn.fodselsdato;
-
         personinfo
                 .getFamilierelasjoner()
                 .stream()
@@ -89,14 +94,8 @@ public class Oppslag {
 
     private String hentAktørId(String personident){
         URI uri = URI.create(oppslagServiceUri + "/aktoer?ident=" + personident);
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(uri)
-                .header("Nav-Call-Id", MDC.get(MDCConstants.MDC_CORRELATION_ID))
-                .GET()
-                .build();
         try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request(uri), HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() != 200) {
                 logger.info("Kall mot oppslag feilet: " + response.body());
                 throw new OppslagException(response.body());
@@ -111,13 +110,8 @@ public class Oppslag {
 
     private PersonhistorikkInfo hentHistorikkFor(String aktørId) {
         URI uri = URI.create(oppslagServiceUri + "/personopplysning/historikk?id=" + aktørId);
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(uri)
-                .header("Nav-Call-Id", MDC.get(MDCConstants.MDC_CORRELATION_ID))
-                .GET()
-                .build();
         try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request(uri), HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() != 200) {
                 logger.info("Kall mot oppslag feilet: " + response.body());
                 throw new OppslagException(response.body());
