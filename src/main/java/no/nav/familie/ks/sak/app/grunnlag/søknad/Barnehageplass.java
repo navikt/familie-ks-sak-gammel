e7f632e7eb11156d48d9eff1fd750c8781d44e03
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-08-06 10:19:03 +0200

    [kaizen] Korriger enumverdi

--- src/main/java/no/nav/familie/ks/sak/app/grunnlag/søknad/Barnehageplass.java
+++ src/main/java/no/nav/familie/ks/sak/app/grunnlag/søknad/Barnehageplass.java
@@ -29,7 +29,7 @@ public class Barnehageplass {
 
     public enum BarnehageplassVerdier {
         garIkkeIBarnehage,
-        garIBarnehage,
+        harBarnehageplass,
         harSluttetIBarnehage,
         skalBegynneIBarnehage,
         skalSlutteIBarnehage
