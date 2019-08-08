e913633bc64e3b4d51c370fd6715477c33c2a826
Henning Håkonsen <henninghaakonsen@me.com>
2019-08-08 13:21:49 +0200

    Skriver om logikk for å hente ut statsborgerskal etter bug med nullpointer.

--- src/main/java/no/nav/familie/ks/sak/app/behandling/regler/SjekkMedlemsskap.java
+++ src/main/java/no/nav/familie/ks/sak/app/behandling/regler/SjekkMedlemsskap.java
@@ -7,12 +7,14 @@ import no.nav.familie.ks.sak.app.integrasjon.personopplysning.domene.Personhisto
 import no.nav.familie.ks.sak.app.integrasjon.personopplysning.domene.adresse.AdressePeriode;
 import no.nav.familie.ks.sak.app.integrasjon.personopplysning.domene.adresse.AdresseType;
 import no.nav.familie.ks.sak.app.integrasjon.personopplysning.domene.tilhørighet.Landkode;
+import no.nav.familie.ks.sak.app.integrasjon.personopplysning.domene.tilhørighet.StatsborgerskapPeriode;
 import no.nav.fpsak.nare.doc.RuleDocumentation;
 import no.nav.fpsak.nare.evaluation.Evaluation;
 import no.nav.fpsak.nare.specification.LeafSpecification;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Optional;
 
 @RuleDocumentation(SjekkMedlemsskap.ID)
 public class SjekkMedlemsskap extends LeafSpecification<Faktagrunnlag> {
@@ -30,31 +32,31 @@ public class SjekkMedlemsskap extends LeafSpecification<Faktagrunnlag> {
     public Evaluation evaluate(Faktagrunnlag grunnlag) {
         Forelder forelder = grunnlag.getTpsFakta().getForelder();
         Forelder annenForelder = grunnlag.getTpsFakta().getAnnenForelder();
-        if (! norskMedlemsskap(forelder.getPersonhistorikkInfo())
-                || (annenForelder != null && ! norskMedlemsskap(annenForelder.getPersonhistorikkInfo()))) {
+        if (!norskMedlemsskap(forelder.getPersonhistorikkInfo())
+                || (annenForelder != null && !norskMedlemsskap(annenForelder.getPersonhistorikkInfo()))) {
             return ja();
         }
         return nei();
     }
 
     private boolean norskMedlemsskap(PersonhistorikkInfo personhistorikkInfo) {
-        Landkode statsborgerskap = personhistorikkInfo.getStatsborgerskaphistorikk().stream()
-                .filter( periode -> periode.getPeriode().getTom().equals(Tid.TIDENES_ENDE))
-                .findFirst()
-                .get()
-                .getTilhørendeLand();
+        Optional<Landkode> statsborgerskap = personhistorikkInfo.getStatsborgerskaphistorikk().stream()
+                .filter(periode -> periode.getPeriode().getTom().equals(Tid.TIDENES_ENDE))
+                .filter(periode -> periode.getTilhørendeLand().erNorge())
+                .map(StatsborgerskapPeriode::getTilhørendeLand)
+                .findFirst();
         int antallMånederINorge = personhistorikkInfo.getAdressehistorikk().stream()
                 .filter(this::erNorskBostedsadresse)
                 .map(AdressePeriode::getPeriode)
-                .map( periode -> periode.getTom().equals(Tid.TIDENES_ENDE)
+                .map(periode -> periode.getTom().equals(Tid.TIDENES_ENDE)
                         ? Period.between(periode.getFom(), LocalDate.now())
                         : Period.between(periode.getFom(), periode.getTom()))
-                .map( periode -> periode.getYears() * ANTALL_MÅNEDER_I_ÅRET + periode.getMonths())
+                .map(periode -> periode.getYears() * ANTALL_MÅNEDER_I_ÅRET + periode.getMonths())
                 .mapToInt(Integer::intValue)
                 .sum();
 
         boolean boddINorgeFemÅr = antallMånederINorge >= MIN_ANTALL_ÅR * ANTALL_MÅNEDER_I_ÅRET;
-        return statsborgerskap.erNorge() && boddINorgeFemÅr;
+        return statsborgerskap.isPresent() && boddINorgeFemÅr;
     }
 
     private boolean erNorskBostedsadresse(AdressePeriode adressePeriode) {
