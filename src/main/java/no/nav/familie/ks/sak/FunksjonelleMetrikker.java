6b3bfa65b70808e64d1dfe9043c982025c885b32
Henning Håkonsen <henning.hakonsen@nav.no>
2019-08-08 15:46:05 +0200

    Forbedrer funksjonelle metrikker (#11)
    * Refaktorert metrikker og forbedret gyldige verdier.
    
    * Gir metrikker bedre navn.
    
    * Forbedret navn på variabler for metrikker. Fikset bug i metrikker ved oppfylt søknad.
    
    * Skriver om logikk for å hente ut statsborgerskal etter bug med nullpointer.
    
    * Setter unprotected på endepunktet for testing med postman.
    
    * Fjerner unprotected.

--- src/main/java/no/nav/familie/ks/sak/FunksjonelleMetrikker.java
+++ src/main/java/no/nav/familie/ks/sak/FunksjonelleMetrikker.java
@@ -4,39 +4,59 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
+import no.nav.familie.ks.sak.app.behandling.resultat.UtfallType;
+import no.nav.familie.ks.sak.app.behandling.resultat.Vedtak;
+import no.nav.familie.ks.sak.app.behandling.resultat.årsak.VilkårIkkeOppfyltÅrsak;
 import no.nav.familie.ks.sak.app.grunnlag.Søknad;
 import no.nav.familie.ks.sak.app.grunnlag.søknad.Barnehageplass;
+import no.nav.familie.ks.sak.app.grunnlag.søknad.TilknytningTilUtland;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 
 @Component
 public class FunksjonelleMetrikker {
+    private final HashMap<String, Counter> søkerUtfall = Maps.newHashMap();
+    private final HashMap<String, Counter> vilkårIkkeOppfylt = Maps.newHashMap();
 
-    private final HashMap<String, Counter> barnehageCounters = Maps.newHashMap();
+    private final HashMap<String, Counter> barnehagestatus = Maps.newHashMap();
+    private final HashMap<String, Counter> boddEllerJobbetINorgeEllerEøsIFemÅr = Maps.newHashMap();
     private final Counter mottarKontantstotteFraUtlandet = Metrics.counter("soknad.kontantstotte.funksjonell.mottarKontantstotteFraUtlandet", "status", "JA");
-    private final Counter boddEllerJobbetINorgeMinstFemAar = Metrics.counter("soknad.kontantstotte.funksjonell.boddEllerJobbetINorgeMinstFemAar", "status", "JA");
-    private final Counter ikkeBoddEllerJobbetINorgeMinstFemAar = Metrics.counter("soknad.kontantstotte.funksjonell.boddEllerJobbetINorgeMinstFemAar", "status", "NEI");
 
 
     public FunksjonelleMetrikker() {
-        Lists.newArrayList(Barnehageplass.BarnehageplassVerdier.values()).forEach(barnehageplassVerdi -> barnehageCounters.put(
+        Lists.newArrayList(UtfallType.values()).forEach(utfallType -> søkerUtfall.put(
+                utfallType.name(),
+                Metrics.counter("soknad.kontantstotte.behandling.funksjonell.utfall", "status", utfallType.name(), "beskrivelse", utfallType.getBeskrivelse())
+        ));
+
+        Lists.newArrayList(VilkårIkkeOppfyltÅrsak.values()).forEach(vilkårIkkeOppfyltÅrsak -> vilkårIkkeOppfylt.put(
+                Integer.toString(vilkårIkkeOppfyltÅrsak.getÅrsakKode()),
+                Metrics.counter("soknad.kontantstotte.behandling.funksjonell.avslag", "status", Integer.toString(vilkårIkkeOppfyltÅrsak.getÅrsakKode()), "beskrivelse", vilkårIkkeOppfyltÅrsak.getBeskrivelse())
+        ));
+
+        Lists.newArrayList(Barnehageplass.BarnehageplassVerdier.values()).forEach(barnehageplassVerdi -> barnehagestatus.put(
                 barnehageplassVerdi.name(),
                 Metrics.counter("soknad.kontantstotte.funksjonell.barnehage", "status", barnehageplassVerdi.name(), "beskrivelse", barnehageplassVerdi.getBeskrivelse())
         ));
+
+        Lists.newArrayList(TilknytningTilUtland.TilknytningTilUtlandVerdier.values()).forEach(tilknytningTilUtlandVerdi -> boddEllerJobbetINorgeEllerEøsIFemÅr.put(
+                tilknytningTilUtlandVerdi.name(),
+                Metrics.counter("soknad.kontantstotte.funksjonell.boddEllerJobbetINorgeEllerEøsIFemÅr", "status", tilknytningTilUtlandVerdi.name(), "beskrivelse", tilknytningTilUtlandVerdi.getBeskrivelse())
+        ));
     }
 
-    public void tellFunksjonelleMetrikker(Søknad søknad) {
-        barnehageCounters.get(søknad.barnehageplass.barnBarnehageplassStatus.name()).increment();
+    public void tellFunksjonelleMetrikker(Søknad søknad, Vedtak vedtak) {
+        søkerUtfall.get(vedtak.getVilkårvurdering().getUtfallType().name()).increment();
+        if (vedtak.getVilkårvurdering().getUtfallType().equals(UtfallType.IKKE_OPPFYLT)) {
+            vilkårIkkeOppfylt.get(Integer.toString(vedtak.getVilkårvurdering().getVilkårÅrsak().getÅrsakKode())).increment();
+        }
+
+        barnehagestatus.get(søknad.barnehageplass.barnBarnehageplassStatus.name()).increment();
+        boddEllerJobbetINorgeEllerEøsIFemÅr.get(søknad.tilknytningTilUtland.boddEllerJobbetINorgeMinstFemAar.name()).increment();
 
         if (søknad.utenlandskKontantstotte.mottarKontantstotteFraUtlandet.equals("JA")) {
             mottarKontantstotteFraUtlandet.increment();
-        }
-
-        if (søknad.tilknytningTilUtland.boddEllerJobbetINorgeMinstFemAar.equals("JA")) {
-            boddEllerJobbetINorgeMinstFemAar.increment();
-        } else {
-            ikkeBoddEllerJobbetINorgeMinstFemAar.increment();
         }
     }
 }
--- src/main/java/no/nav/familie/ks/sak/app/behandling/MottaSøknadController.java
+++ src/main/java/no/nav/familie/ks/sak/app/behandling/MottaSøknadController.java
@@ -1,17 +1,11 @@
 package no.nav.familie.ks.sak.app.behandling;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.Metrics;
 import no.nav.familie.ks.sak.FunksjonelleMetrikker;
 import no.nav.familie.ks.sak.Saksbehandling;
 import no.nav.familie.ks.sak.app.behandling.resultat.UtfallType;
 import no.nav.familie.ks.sak.app.behandling.resultat.Vedtak;
-import no.nav.familie.ks.sak.app.behandling.resultat.årsak.VilkårIkkeOppfyltÅrsak;
 import no.nav.familie.ks.sak.app.grunnlag.Søknad;
 import no.nav.security.oidc.api.ProtectedWithClaims;
-import no.nav.security.oidc.api.Unprotected;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,18 +14,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 @RestController
 @RequestMapping("/api/mottak")
 @ProtectedWithClaims(issuer = "intern")
 public class MottaSøknadController {
 
     private static final Logger log = LoggerFactory.getLogger(MottaSøknadController.class);
-    private final Counter sokerKanBehandlesAutomatisk = Metrics.counter("soknad.kontantstotte.behandling.automatisk", "status", "JA", "beskrivelse", "Søknaden kan automatisk godkjennes");
-    private final Counter sokerKanIkkeBehandlesAutomatisk = Metrics.counter("soknad.kontantstotte.behandling.automatisk", "status", "NEI", "beskrivelse", "Søknanden kan ikke automatisk godkjennes");
-    private final HashMap<String, Counter> vilkårIkkeOppfyltCounters = Maps.newHashMap();
 
     private final FunksjonelleMetrikker funksjonelleMetrikker;
     private final Saksbehandling saksbehandling;
@@ -39,12 +27,6 @@ public class MottaSøknadController {
     public MottaSøknadController(@Autowired Saksbehandling saksbehandling, @Autowired FunksjonelleMetrikker funksjonelleMetrikker) {
         this.funksjonelleMetrikker = funksjonelleMetrikker;
         this.saksbehandling = saksbehandling;
-
-
-        Lists.newArrayList(VilkårIkkeOppfyltÅrsak.values()).forEach(vilkårIkkeOppfyltÅrsak -> vilkårIkkeOppfyltCounters.put(
-                Integer.toString(vilkårIkkeOppfyltÅrsak.getÅrsakKode()),
-                Metrics.counter("soknad.kontantstotte.behandling.automatisk.avslag", "status", Integer.toString(vilkårIkkeOppfyltÅrsak.getÅrsakKode()), "beskrivelse", vilkårIkkeOppfyltÅrsak.getBeskrivelse())
-        ));
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, path = "dokument")
@@ -55,16 +37,13 @@ public class MottaSøknadController {
             log.info("Søknad kan behandles automatisk. Årsak " +
                     vedtak.getVilkårvurdering().getVilkårÅrsak().getÅrsakKode() + ": " +
                     vedtak.getVilkårvurdering().getVilkårÅrsak().getBeskrivelse());
-            sokerKanBehandlesAutomatisk.increment();
         } else {
             log.info("Søknad kan ikke behandles automatisk. Årsak " +
                     vedtak.getVilkårvurdering().getVilkårÅrsak().getÅrsakKode() + ": " +
                     vedtak.getVilkårvurdering().getVilkårÅrsak().getBeskrivelse());
-            vilkårIkkeOppfyltCounters.get(Integer.toString(vedtak.getVilkårvurdering().getVilkårÅrsak().getÅrsakKode())).increment();
-            sokerKanIkkeBehandlesAutomatisk.increment();
         }
 
-        funksjonelleMetrikker.tellFunksjonelleMetrikker(søknad);
+        funksjonelleMetrikker.tellFunksjonelleMetrikker(søknad, vedtak);
 
         return new ResponseEntity(HttpStatus.OK);
     }
