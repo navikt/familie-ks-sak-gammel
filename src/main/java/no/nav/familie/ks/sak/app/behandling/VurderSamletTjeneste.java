088db5cb26d6fb64dee4895938c9be7524edc399
Marius Glittum <marius.glittum@nav.no>
2019-08-14 13:58:34 +0200

    Minor typos

--- src/main/java/no/nav/familie/ks/sak/app/behandling/VurderSamletTjeneste.java
+++ src/main/java/no/nav/familie/ks/sak/app/behandling/VurderSamletTjeneste.java
@@ -21,13 +21,14 @@ public class VurderSamletTjeneste {
         this.inngangsvilkår = inngangsvilkår;
     }
 
+    @SuppressWarnings("unchecked")
     public SamletVilkårsVurdering vurder(Faktagrunnlag grunnlag) {
         final Map<VilkårType, Evaluation> vurderinger = new HashMap<>();
         inngangsvilkår.forEach(vilkår -> {
             final var input = vilkår.konverterInput(grunnlag);
             final var evaluering = vilkår.evaluer(input);
             if (vurderinger.containsKey(vilkår.getVilkårType())) {
-                throw new IllegalStateException("Fant flere inngangsvilkår med samme vilkårsreferanse");
+                throw new IllegalStateException("Fant flere inngangsvilkår med samme kode");
             }
             vurderinger.put(vilkår.getVilkårType(), evaluering);
         });
