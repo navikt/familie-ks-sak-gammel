401d825be9ef5fe0525c5af5a20d2b91c12900b1
Marius Glittum <marius.glittum@nav.no>
2019-08-20 12:32:57 +0200

    Utkast modell

---
+++ src/main/resources/db/migration/V2__personopplysninger.sql
@@ -0,0 +1,25 @@
+create table GR_PERSONOPPLYSNINGER
+(
+    ID                 serial primary key,
+    BEHANDLING_ID      bigint references BEHANDLING (id)   not null,
+    OPPGITT_ANNEN_PART VARCHAR(50),
+    VERSJON            bigint       default 0              not null,
+    OPPRETTET_AV       VARCHAR(20)  default 'VL'           not null,
+    OPPRETTET_TID      TIMESTAMP(3) default localtimestamp not null,
+    ENDRET_AV          VARCHAR(20),
+    ENDRET_TID         TIMESTAMP(3),
+    AKTIV              boolean      default true           not null
+);
+
+CREATE UNIQUE INDEX UIDX_GR_SOKNAD_01
+    ON GR_PERSONOPPLYSNINGER
+        (
+         (CASE
+              WHEN AKTIV = true
+                  THEN BEHANDLING_ID
+              ELSE NULL END),
+         (CASE
+              WHEN AKTIV = true
+                  THEN AKTIV
+              ELSE NULL END)
+            );
