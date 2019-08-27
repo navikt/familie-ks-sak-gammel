ea065c50129f3a24aec1caebc789575cd41ab628
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-08-27 10:33:58 +0200

    Fix typo

--- src/test/java/no/nav/familie/ks/sak/app/behandling/vilkår/MedlemskapVilkårTest.java
+++ src/test/java/no/nav/familie/ks/sak/app/behandling/vilkår/MedlemskapVilkårTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class MedlemskapsVilkårTest {
+public class MedlemskapVilkårTest {
 
     @Test
     public void vurderer_begge_delvilkår() {
