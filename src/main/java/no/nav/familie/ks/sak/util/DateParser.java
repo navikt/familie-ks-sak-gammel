f74ecba9d74e3656d402a8089478348319298f99
Henning Håkonsen <henninghaakonsen@me.com>
2019-09-02 12:44:25 +0200

    Endrer forventet format på innkommende datofelt fra søknad.

--- src/main/java/no/nav/familie/ks/sak/util/DateParser.java
+++ src/main/java/no/nav/familie/ks/sak/util/DateParser.java
@@ -1,9 +1,10 @@
 package no.nav.familie.ks.sak.util;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DateParser {
     public static LocalDate parseSøknadDato(String dato) {
-        return LocalDate.parse(dato);
+        return LocalDate.parse(dato, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 }
