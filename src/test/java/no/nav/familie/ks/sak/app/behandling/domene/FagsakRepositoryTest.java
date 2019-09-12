9548c56f0e40a9377709e556186e3e04f8e170dd
Henning Solberg <henning.solberg@nav.no>
2019-09-12 18:23:47 +0200

    Lagt inn test som som viser feilen, TODO rette feilen

--- src/test/java/no/nav/familie/ks/sak/app/behandling/domene/FagsakRepositoryTest.java
+++ src/test/java/no/nav/familie/ks/sak/app/behandling/domene/FagsakRepositoryTest.java
@@ -1,22 +1,39 @@
 package no.nav.familie.ks.sak.app.behandling.domene;
 
+import no.nav.familie.http.sts.StsRestClient;
 import no.nav.familie.ks.sak.ApplicationTestPropertyValues;
 import no.nav.familie.ks.sak.DevLauncher;
+import no.nav.familie.ks.sak.FaktagrunnlagBuilder;
+import no.nav.familie.ks.sak.Saksbehandling;
+import no.nav.familie.ks.sak.app.behandling.BehandlingslagerService;
+import no.nav.familie.ks.sak.app.behandling.domene.grunnlag.barnehagebarn.BarnehageBarnGrunnlagRepository;
+import no.nav.familie.ks.sak.app.behandling.domene.grunnlag.søknad.SøknadGrunnlagRepository;
 import no.nav.familie.ks.sak.app.behandling.domene.typer.AktørId;
+import no.nav.familie.ks.sak.app.behandling.resultat.Vedtak;
+import no.nav.familie.ks.sak.app.integrasjon.OppslagTjeneste;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringRunner.class)
@@ -25,20 +42,55 @@ import static org.junit.Assert.assertEquals;
 @ContextConfiguration(classes = DevLauncher.class, initializers = { FagsakRepositoryTest.Initializer.class })
 public class FagsakRepositoryTest {
 
+    @MockBean
+    private OppslagTjeneste oppslagTjeneste;
+    @MockBean
+    private StsRestClient stsRestClient;
+
+    @Autowired
+    private BehandlingslagerService tjeneste;
+
+    @Autowired
+    private FagsakRepository fagsakRepository;
+    @Autowired
+    private BehandlingRepository behandlingRepository;
+    @Autowired
+    private SøknadGrunnlagRepository søknadGrunnlagRepository;
+    @Autowired
+    private BarnehageBarnGrunnlagRepository barnehageBarnGrunnlagRepository;
+
+    @Autowired
+    private Saksbehandling saksbehandling;
+
+    @Before
+    public void setUp() throws Exception {
+        Mockito.when(oppslagTjeneste.hentAktørId(ArgumentMatchers.any())).thenAnswer(i -> new AktørId(String.valueOf(i.getArguments()[0])));
+        when(oppslagTjeneste.hentTpsFakta(any(), any(), any())).thenReturn(FaktagrunnlagBuilder.faktaBeggeForeldreOgBarnNorskStatsborger());
+        when(oppslagTjeneste.hentPersoninfoFor(any())).thenReturn(FaktagrunnlagBuilder.faktaBeggeForeldreOgBarnNorskStatsborger().getForelder().getPersoninfo(),
+            FaktagrunnlagBuilder.faktaBeggeForeldreOgBarnNorskStatsborger().getAnnenForelder().getPersoninfo(),
+            FaktagrunnlagBuilder.faktaBeggeForeldreOgBarnNorskStatsborger().getBarn().getPersoninfo());
+        when(oppslagTjeneste.hentHistorikkFor(any())).thenReturn(FaktagrunnlagBuilder.faktaBeggeForeldreOgBarnNorskStatsborger().getForelder().getPersonhistorikkInfo(),
+            FaktagrunnlagBuilder.faktaBeggeForeldreOgBarnNorskStatsborger().getAnnenForelder().getPersonhistorikkInfo(),
+            FaktagrunnlagBuilder.faktaBeggeForeldreOgBarnNorskStatsborger().getBarn().getPersonhistorikkInfo());
+    }
+
     @ClassRule
     public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
 
-    @Autowired
-    private FagsakRepository repository;
 
     @Test
-    public void shouldStoreEachNotification() {
+    public void skal_kjøre_flyway_migreringer_og_lagre_data_i_postgresql() {
 
-        // given
-        repository.save(new Fagsak(new AktørId("sdf"), "444"));
+        //given
+        final var søknad = FaktagrunnlagBuilder.utenBarnehageplass();
+        Vedtak vedtak = saksbehandling.behandle(søknad);
+        Optional<Behandling> behandling = behandlingRepository.findById(vedtak.getBehandlingsId());
+
+        assertThat(behandling).isPresent();
+
 
         // when
-        long count = repository.count();
+        long count = fagsakRepository.count();
 
         // then
         assertEquals(count, 1);
