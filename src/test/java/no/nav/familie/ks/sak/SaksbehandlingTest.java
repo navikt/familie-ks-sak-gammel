9de03a637d02d8ab2db5851fdafa713615c703c3
Geirmund Simonsen <geirmund.simonsen@gmail.com>
2019-09-16 09:52:18 +0200

    Behandle(string) kaller behandle(søknad) (#44)
    * Saksbehandling.java: behandle(String) kaller behandle(Søknad)
    
    * Rett opp i tester som feiler etter funksjonssammenslåing

--- src/test/java/no/nav/familie/ks/sak/SaksbehandlingTest.java
+++ src/test/java/no/nav/familie/ks/sak/SaksbehandlingTest.java
@@ -3,7 +3,10 @@ package no.nav.familie.ks.sak;
 import no.nav.familie.ks.sak.app.behandling.BehandlingslagerService;
 import no.nav.familie.ks.sak.app.behandling.ResultatService;
 import no.nav.familie.ks.sak.app.behandling.VurderSamletTjeneste;
+import no.nav.familie.ks.sak.app.behandling.domene.Behandling;
+import no.nav.familie.ks.sak.app.behandling.domene.Fagsak;
 import no.nav.familie.ks.sak.app.behandling.domene.kodeverk.UtfallType;
+import no.nav.familie.ks.sak.app.behandling.domene.typer.AktørId;
 import no.nav.familie.ks.sak.app.behandling.resultat.Vedtak;
 import no.nav.familie.ks.sak.app.behandling.vilkår.medlemskap.MedlemskapsVilkår;
 import no.nav.familie.ks.sak.app.behandling.vilkår.barn.BarneVilkår;
@@ -20,14 +23,15 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class SaksbehandlingTest {
-
     private final OppslagTjeneste oppslagMock = mock(OppslagTjeneste.class);
+    private final BehandlingslagerService behandlingslagerMock = mock(BehandlingslagerService.class);
     private final VurderSamletTjeneste vurderSamletTjeneste = new VurderSamletTjeneste(List.of(new BarneVilkår(), new MedlemskapsVilkår()));
-    private final Saksbehandling saksbehandling = new Saksbehandling(oppslagMock, vurderSamletTjeneste, mock(BehandlingslagerService.class), mock(RegisterInnhentingService.class), mock(ResultatService.class), new JacksonJsonConfig().objectMapper());
+    private final Saksbehandling saksbehandling = new Saksbehandling(oppslagMock, vurderSamletTjeneste, behandlingslagerMock, mock(RegisterInnhentingService.class), mock(ResultatService.class), new JacksonJsonConfig().objectMapper());
 
     @Test
     public void positivt_vedtak_ved_oppfylte_vilkår() {
         when(oppslagMock.hentTpsFakta(any(), any(), any())).thenReturn(FaktagrunnlagBuilder.faktaBeggeForeldreOgBarnNorskStatsborger());
+        when(behandlingslagerMock.trekkUtOgPersister(any())).thenReturn(Behandling.forFørstegangssøknad(new Fagsak(new AktørId(0L), "")).build());
         Vedtak vedtak = saksbehandling.behandle(getFile("soknadUtenBarnehageplass.json"));
 
         assertThat(vedtak.getVilkårvurdering().getSamletUtfallType()).isEqualTo(UtfallType.OPPFYLT);
@@ -36,6 +40,7 @@ public class SaksbehandlingTest {
     @Test
     public void negativt_vedtak_ved_ikke_oppfylte_vilkår() {
         when(oppslagMock.hentTpsFakta(any(), any(), any())).thenReturn(FaktagrunnlagBuilder.faktaBeggeForeldreOgBarnUtenlandskeStatsborgere());
+        when(behandlingslagerMock.trekkUtOgPersister(any())).thenReturn(Behandling.forFørstegangssøknad(new Fagsak(new AktørId(0L), "")).build());
         Vedtak vedtak = saksbehandling.behandle(getFile("soknadGradertBarnehageplass.json"));
 
         assertThat(vedtak.getVilkårvurdering().getSamletUtfallType()).isEqualTo(UtfallType.IKKE_OPPFYLT);
