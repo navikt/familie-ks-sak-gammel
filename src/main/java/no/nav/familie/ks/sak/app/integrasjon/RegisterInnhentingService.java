36c84a5e7dc6a5c732be5889c29aa17b173e43ad
Henning Håkonsen <henninghaakonsen@me.com>
2019-09-18 09:19:17 +0200

    Endrer oppslagstjenesten slik at personident er med i responsen.

--- src/main/java/no/nav/familie/ks/sak/app/integrasjon/RegisterInnhentingService.java
+++ src/main/java/no/nav/familie/ks/sak/app/integrasjon/RegisterInnhentingService.java
@@ -70,7 +70,7 @@ public class RegisterInnhentingService {
             if (annenPartFamilierelasjon.isPresent()) {
                 AktørId annenPartAktørId = annenPartFamilierelasjon.get().getAktørId();
                 annenPartPersonMedHistorikk = hentPersonMedHistorikk(annenPartAktørId);
-                String annenPartPersonIdent = oppslagTjeneste.hentPersonIdent(annenPartAktørId.getId()).getIdent();
+                String annenPartPersonIdent = annenPartPersonMedHistorikk.getPersoninfo().getPersonIdent().getIdent();
 
                 if (annenPartPersonIdent.regionMatches(0, oppgittAnnenPartPersonIdent, 0, 6)) {
                     if (!annenPartPersonIdent.equals(oppgittAnnenPartPersonIdent)) {
