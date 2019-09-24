5318208030d04dacc819081f7aa5ec636bd3ae9d
Geirmund Simonsen <geirmundsimonsen@Geirmunds-MacBook-Pro.local>
2019-09-24 15:07:40 +0200

    - Slett SaksbehandlingsTest - Gjør testene i samletVilkårsVurderingsTest mer generelle.

--- src/test/java/no/nav/familie/ks/sak/app/behandling/SamletVilkårsVurderingTest.java
+++ src/test/java/no/nav/familie/ks/sak/app/behandling/SamletVilkårsVurderingTest.java
@@ -26,34 +26,31 @@ public class SamletVilkårsVurderingTest {
         final var faktagrunnlag = FaktagrunnlagBuilder.familieNorskStatsborgerskapUtenBarnehage();
         final var vurder = vurderSamletTjeneste.vurder(faktagrunnlag);
         final var alleUtfall = vurder.getResultater().stream().map(Regelresultat::getUtfallType).collect(Collectors.toList());
+
         assertThat(alleUtfall).hasSize(inngangsvilkår.size());
-        assertThat(alleUtfall).containsExactlyInAnyOrder(UtfallType.OPPFYLT, UtfallType.OPPFYLT, UtfallType.OPPFYLT, UtfallType.OPPFYLT, UtfallType.OPPFYLT);
+        assertThat(alleUtfall).containsOnly(UtfallType.OPPFYLT);
         assertThat(vurder.getSamletUtfallType()).isEqualTo(UtfallType.OPPFYLT);
     }
 
     @Test
     public void gir_ikke_oppfylt_når_ikke_alle_er_oppfylt() {
         final var faktagrunnlag = FaktagrunnlagBuilder.familieNorskStatsborgerskapMedBarnehage();
-
         final var vurder = vurderSamletTjeneste.vurder(faktagrunnlag);
-
         final var alleUtfall = vurder.getResultater().stream().map(Regelresultat::getUtfallType).collect(Collectors.toList());
-        assertThat(alleUtfall).hasSize(inngangsvilkår.size());
-        assertThat(alleUtfall).containsExactlyInAnyOrder(UtfallType.IKKE_OPPFYLT, UtfallType.OPPFYLT, UtfallType.OPPFYLT, UtfallType.OPPFYLT,  UtfallType.OPPFYLT);
-        assertThat(vurder.getSamletUtfallType()).isEqualTo(UtfallType.IKKE_OPPFYLT);
 
+        assertThat(alleUtfall).hasSize(inngangsvilkår.size());
+        assertThat(alleUtfall).containsAnyOf(UtfallType.IKKE_OPPFYLT);
+        assertThat(vurder.getSamletUtfallType()).isEqualTo(UtfallType.IKKE_OPPFYLT);
     }
 
     @Test
     public void har_utfall_årsak_på_alle_regel_resultater() {
         final var faktagrunnlag = FaktagrunnlagBuilder.familieNorskStatsborgerskapMedBarnehage();
-
         final var vurder = vurderSamletTjeneste.vurder(faktagrunnlag);
         final var resultater = vurder.getResultater();
 
         for (Regelresultat resultat : resultater) {
             assertThat(resultat.getUtfallÅrsak()).isNotNull();
         }
-
     }
 }
