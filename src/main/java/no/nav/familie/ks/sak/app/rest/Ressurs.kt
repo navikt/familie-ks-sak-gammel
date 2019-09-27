f9cff8bbc13a74373bcaa33e3fd371aaf634c409
Henning Håkonsen <henninghaakonsen@me.com>
2019-09-27 14:25:46 +0200

    Henter bare ut melding fra feil i en ressurs.

--- src/main/java/no/nav/familie/ks/sak/app/rest/Ressurs.kt
+++ src/main/java/no/nav/familie/ks/sak/app/rest/Ressurs.kt
@@ -2,7 +2,7 @@ package no.nav.familie.ks.sak.app.rest
 
 import com.fasterxml.jackson.databind.JsonNode
 
-data class Ressurs(val data: JsonNode?, val status: Status, val melding: String, val error: Throwable?) {
+data class Ressurs(val data: JsonNode?, val status: Status, val melding: String, val errorMelding: String?) {
     enum class Status { SUKSESS, FEILET, IKKE_HENTET }
 
     companion object {
@@ -10,14 +10,14 @@ data class Ressurs(val data: JsonNode?, val status: Status, val melding: String,
             data = data,
             status = Status.SUKSESS,
             melding = "Innhenting av data var vellykket",
-            error = null
+            errorMelding = null
         )
 
         fun failure(errorMessage: String? = null, error: Throwable? = null) = Ressurs(
             data = null,
             status = Status.FEILET,
             melding = errorMessage ?: "Kunne ikke hente data: ${error?.message}",
-            error = error
+            errorMelding = error?.message
         )
     }
 }
