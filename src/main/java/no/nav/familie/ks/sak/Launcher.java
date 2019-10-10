c82a37b8bb6fdf6842fe0230154dc58b00d60fb4
Henning Håkonsen <henning.hakonsen@nav.no>
2019-10-10 13:31:13 +0100

    Verifiserer azure tokens (#77)
    * Forbedringer etter integrasjon mellom api og frontend.
    
    * Setter fagsak endepunkter til midlertidig unprotected.
    
    * Endre til å bruke saksnummer for henting av fagsak.
    
    * Fjerner unprotected fra tjenester for frontend.
    
    * Legger til auditloggingen som ble fjernet ved merge.
    
    * Henter ut koden fra landkode.
    
    * Bumper oidc dependency. Legger til azure ad issuer.
    
    * Legger inn konfigurasjon for azure issuer.
    
    * Sett proxy kun for azure ad well known endepunkt.
    
    * Setter riktig host for proxy.
    
    * Setter logging til debug for jwt validering.
    
    * Fjerner debug logging.

--- src/main/java/no/nav/familie/ks/sak/Launcher.java
+++ src/main/java/no/nav/familie/ks/sak/Launcher.java
@@ -1,16 +1,13 @@
 package no.nav.familie.ks.sak;
 
+import no.nav.familie.ks.sak.config.ApplicationConfig;
+import no.nav.familie.ks.sak.config.DelayedShutdownHook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import no.nav.familie.ks.sak.config.ApplicationConfig;
-import no.nav.familie.ks.sak.config.DelayedShutdownHook;
-import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
-
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-@EnableOIDCTokenValidation(ignore = "org.springframework")
 public class Launcher {
 
     public static void main(String... args) {
