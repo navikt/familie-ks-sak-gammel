293debf480ae03927afc94ca8cc90f3011fcd401
Henning Håkonsen <henninghaakonsen@me.com>
2019-10-14 15:05:33 +0200

    Legger til opprettet tidspunkt for fagsaken.

--- src/main/java/no/nav/familie/ks/sak/app/rest/behandling/RestFagsak.kt
+++ src/main/java/no/nav/familie/ks/sak/app/rest/behandling/RestFagsak.kt
@@ -1,16 +1,17 @@
 package no.nav.familie.ks.sak.app.rest.behandling
 
 import no.nav.familie.ks.sak.app.behandling.domene.Fagsak
-import no.nav.familie.ks.sak.app.integrasjon.OppslagTjeneste
-import no.nav.familie.ks.sak.app.integrasjon.personopplysning.domene.PersonIdent
+import java.time.LocalDateTime
 
 data class RestFagsak(
+    val opprettetTidspunkt: LocalDateTime,
     val id: Long,
     val søkerFødselsnummer: String,
     val saksnummer: String,
     val behandlinger: List<RestBehandling>)
 
 fun Fagsak.toRestFagsak(restBehandlinger: List<RestBehandling>, søkerFødselsnummer: String) = RestFagsak(
+    opprettetTidspunkt = this.opprettetTidspunkt,
     id = this.id,
     søkerFødselsnummer = søkerFødselsnummer,
     saksnummer = this.saksnummer,
