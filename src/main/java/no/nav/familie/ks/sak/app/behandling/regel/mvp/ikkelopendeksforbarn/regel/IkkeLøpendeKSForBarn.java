a31bc755b1e249c4a2e5a3bde72230539dae33b6
Geirmund Simonsen <geirmundsimonsen@Geirmunds-MacBook-Pro.local>
2019-10-18 12:16:38 +0200

    legg til regel for at barn ikke kan ha løpende kontantstøtte.

---
+++ src/main/java/no/nav/familie/ks/sak/app/behandling/regel/mvp/ikkelopendeksforbarn/regel/IkkeLøpendeKSForBarn.java
@@ -0,0 +1,30 @@
+package no.nav.familie.ks.sak.app.behandling.regel.mvp.ikkelopendeksforbarn.regel;
+
+import no.nav.familie.ks.sak.app.behandling.fastsetting.Faktagrunnlag;
+import no.nav.fpsak.nare.doc.RuleDocumentation;
+import no.nav.fpsak.nare.evaluation.Evaluation;
+import no.nav.fpsak.nare.specification.LeafSpecification;
+
+@RuleDocumentation(IkkeLøpendeKSForBarn.ID)
+public class IkkeLøpendeKSForBarn extends LeafSpecification<Faktagrunnlag> {
+    
+    public static final String ID = "IKKE_LØPENDE_KS_FOR_BARN";
+
+    public IkkeLøpendeKSForBarn() {
+        super(ID);
+    }
+
+    @Override
+    public Evaluation evaluate(Faktagrunnlag faktagrunnlag) {
+        // TODO: Støtte for flerlinger.
+        // TODO: Dette faktapunktet gir true for barn som har løpende kontantstøtte ELLER der saken er under behandling.
+        // Dette er en MVP-regel, som skal erstattes når vi har på plass et endepunkt mot infotrygd som gir mer data.
+        var barnHarLøpendeKontantstøtte = faktagrunnlag.getInfotrygdFakta().getAktivKontantStøtteInfo().getHarAktivKontantstotte();
+        
+        if (!barnHarLøpendeKontantstøtte) {
+            return ja();
+        } else {
+            return nei();
+        }
+    }
+}
