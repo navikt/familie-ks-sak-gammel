295e5f5dba96da086fde794cc7649956f30cf26b
Henning Solberg <henning.solberg@nav.no>
2019-10-20 16:20:44 +0200

    Fjernet import av SecurityContextHolder

--- src/main/java/no/nav/familie/ks/sak/app/rest/FagsakController.kt
+++ src/main/java/no/nav/familie/ks/sak/app/rest/FagsakController.kt
@@ -17,7 +17,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
 
