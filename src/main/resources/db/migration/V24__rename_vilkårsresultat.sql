9a0949be509009725424b6a6b05141ccf4f08afc
Stig Strøm <stig.helge.strom@nav.no>
2019-10-24 09:22:11 +0200

    Rename VilkårResultat til SamletVilkårResultat (#117)
    * Rename VilkårResultat til SamletVilkårResultat
    
    * truncate so tables
    
    * Truncate resten av tabellene

---
+++ src/main/resources/db/migration/V24__rename_vilkårsresultat.sql
@@ -0,0 +1,2 @@
+alter table vilkars_resultat rename to samlet_vilkar_resultat;
+alter table vilkar_resultat rename column vilkars_resultat_id to samlet_vilkar_resultat_id;
---
+++ src/main/resources/db/migration/V25__truncate_so.sql
@@ -0,0 +1,3 @@
+TRUNCATE so_soknad cascade;
+TRUNCATE vilkar_resultat cascade ;
+TRUNCATE samlet_vilkar_resultat cascade;
