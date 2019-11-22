b2af50dd81a48e2b40d2fe9c78d07fe200f65045
Stig Strøm <stig.helge.strom@nav.no>
2019-11-22 09:20:03 +0100

    Tatt i bruk Ressurs på oppslagstjenestene (#148)

--- src/main/java/no/nav/familie/ks/sak/app/integrasjon/OppslagTjeneste.java
+++ src/main/java/no/nav/familie/ks/sak/app/integrasjon/OppslagTjeneste.java
@@ -3,6 +3,7 @@ package no.nav.familie.ks.sak.app.integrasjon;
 import no.nav.familie.http.client.NavHttpHeaders;
 import no.nav.familie.ks.kontrakter.oppgave.Oppgave;
 import no.nav.familie.ks.kontrakter.oppgave.OppgaveKt;
+import no.nav.familie.ks.kontrakter.sak.Ressurs;
 import no.nav.familie.ks.sak.app.behandling.domene.typer.AktørId;
 import no.nav.familie.ks.sak.app.integrasjon.infotrygd.domene.AktivKontantstøtteInfo;
 import no.nav.familie.ks.sak.app.integrasjon.medlemskap.MedlemskapsInfo;
@@ -32,14 +33,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
+import java.util.Map;
+import java.util.Objects;
 
 @Component
 public class OppslagTjeneste extends BaseService {
@@ -69,18 +70,39 @@ public class OppslagTjeneste extends BaseService {
 
         HttpEntity httpEntity = new HttpEntity(headers);
 
-        return getRestTemplate().exchange(uri, HttpMethod.GET, httpEntity, clazz);
+        return request(uri, HttpMethod.GET, clazz, httpEntity);
     }
 
-    private <T> ResponseEntity<T> postRequest(URI uri, String requestBody, Class<T> responseType) {
+    private <T> ResponseEntity<T> postRequest(URI uri, String requestBody, Class<T> clazz) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json;charset=UTF-8");
         headers.add(NavHttpHeaders.NAV_CALLID.asString(), MDC.get(MDCConstants.MDC_CALL_ID));
+        HttpEntity httpEntity = new HttpEntity<>(requestBody, headers);
 
-        return getRestTemplate().exchange(uri, HttpMethod.POST, new HttpEntity<>(requestBody, headers), responseType);
+        return request(uri, HttpMethod.POST, clazz, httpEntity);
     }
 
-    private <T> ResponseEntity<T> requestMedPersonIdent(RestTemplate restTemplate, URI uri, String personident, Class<T> clazz) {
+    private <T> ResponseEntity<T> requestMedPersonIdent(URI uri, String personident, Class<T> clazz) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add(NavHttpHeaders.NAV_CALLID.asString(), MDC.get(MDCConstants.MDC_CALL_ID));
+        headers.add(NavHttpHeaders.NAV_PERSONIDENT.asString(), personident);
+
+        HttpEntity httpEntity = new HttpEntity(headers);
+
+        return request(uri, HttpMethod.GET, clazz, httpEntity);
+    }
+
+    private <T> ResponseEntity<T> requestMedAktørId(URI uri, String aktørId, Class<T> clazz) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add(NavHttpHeaders.NAV_CALLID.asString(), MDC.get(MDCConstants.MDC_CALL_ID));
+        headers.add("Nav-Aktorid", aktørId);
+
+        HttpEntity httpEntity = new HttpEntity(headers);
+        return request(uri, HttpMethod.GET, clazz, httpEntity);
+    }
+
+
+    private <T> ResponseEntity<T> requestUtenRessurs(RestTemplate restTemplate, URI uri, String personident, Class<T> clazz) {
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
         headers.add(NavHttpHeaders.NAV_CALLID.asString(), MDC.get(MDCConstants.MDC_CALL_ID));
         headers.add(NavHttpHeaders.NAV_PERSONIDENT.asString(), personident);
@@ -90,14 +112,12 @@ public class OppslagTjeneste extends BaseService {
         return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, clazz);
     }
 
-    private <T> ResponseEntity<T> requestMedAktørId(URI uri, String aktørId, Class<T> clazz) {
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.add(NavHttpHeaders.NAV_CALLID.asString(), MDC.get(MDCConstants.MDC_CALL_ID));
-        headers.add("Nav-Aktorid", aktørId);
-
-        HttpEntity httpEntity = new HttpEntity(headers);
-
-        return getRestTemplate().exchange(uri, HttpMethod.GET, httpEntity, clazz);
+    private <T> ResponseEntity<T> request(URI uri, HttpMethod method, Class<T> clazz, HttpEntity httpEntity) {
+        var ressursResponse = getRestTemplate().exchange(uri, method, httpEntity, Ressurs.class);
+        if (ressursResponse.getBody() == null) {
+            throw new OppslagException("Response kan ikke være tom", null, uri, null);
+        }
+        return ResponseEntity.status(ressursResponse.getStatusCode()).body(Objects.requireNonNull(ressursResponse.getBody()).convert(clazz));
     }
 
     @Retryable(
@@ -108,24 +128,20 @@ public class OppslagTjeneste extends BaseService {
         if (personident == null || personident.isEmpty()) {
             throw new OppslagException("Ved henting av aktør id er personident null eller tom");
         }
-        URI uri = URI.create(oppslagServiceUri + "/aktoer");
+        URI uri = URI.create(oppslagServiceUri + "/aktoer/v1");
         logger.info("Henter aktørId fra " + oppslagServiceUri);
         try {
-            ResponseEntity<String> response = requestMedPersonIdent(getRestTemplate(), uri, personident, String.class);
+            ResponseEntity<Map> response = requestMedPersonIdent(uri, personident, Map.class);
             secureLogger.info("Vekslet inn fnr: {} til aktørId: {}", personident, response.getBody());
 
-            String aktørId = response.getBody();
+            String aktørId = response.getBody().get("aktørId").toString();
             if (aktørId == null || aktørId.isEmpty()) {
                 throw new OppslagException("AktørId fra oppslagstjenesten er tom");
             } else {
                 return new AktørId(aktørId);
             }
-        } catch (HttpStatusCodeException e) {
-            String feilmelding = Optional.ofNullable(e.getResponseHeaders().getFirst("message")).orElse("Ingen feilmelding");
-            logger.warn("Kall mot oppslag feilet ved uthenting av aktørId: " + feilmelding);
-            throw new OppslagException(feilmelding);
         } catch (RestClientException e) {
-            throw new OppslagException("Ukjent feil ved oppslag mot '" + uri + "'.", e, uri, personident);
+            throw new OppslagException("Kall mot oppslag feilet ved uthenting av aktørId", e, uri, personident);
         }
     }
 
@@ -138,25 +154,20 @@ public class OppslagTjeneste extends BaseService {
         if (aktørId == null || aktørId.isEmpty()) {
             throw new OppslagException("Ved henting av personident er aktørId null eller tom");
         }
-        URI uri = URI.create(oppslagServiceUri + "/aktoer/fraaktorid");
+        URI uri = URI.create(oppslagServiceUri + "/aktoer/v1/fraaktorid");
         logger.info("Henter fnr fra " + oppslagServiceUri);
         try {
-            ResponseEntity<String> response = requestMedAktørId(uri, aktørId, String.class);
+            ResponseEntity<Map> response = requestMedAktørId(uri, aktørId, Map.class);
             secureLogger.info("Vekslet inn aktørId: {} til fnr: {}", aktørId, response.getBody());
 
-            String personIdent = response.getBody();
+            String personIdent = response.getBody().get("personIdent").toString();
             if (personIdent == null || personIdent.isEmpty()) {
                 throw new OppslagException("Personident fra oppslagstjenesten er tom");
             } else {
                 return new PersonIdent(personIdent);
             }
-
-        } catch (HttpStatusCodeException e) {
-            String feilmelding = Optional.ofNullable(e.getResponseHeaders().getFirst("message")).orElse("Ingen feilmelding");
-            logger.warn("Kall mot oppslag feilet ved uthenting av aktørId: " + feilmelding);
-            throw new OppslagException(feilmelding);
         } catch (RestClientException e) {
-            throw new OppslagException("Ukjent feil ved oppslag mot '" + uri + "'.", e, uri, aktørId);
+            throw new OppslagException("Kall mot oppslag feilet ved uthenting av personIdent", e, uri, aktørId);
         }
     }
 
@@ -171,7 +182,7 @@ public class OppslagTjeneste extends BaseService {
         URI uri = URI.create(oppslagServiceUri + "/tilgang/person");
         logger.info("Sjekker tilgang  " + oppslagServiceUri);
         try {
-            ResponseEntity<Tilgang> response = requestMedPersonIdent(restTemplate, uri, personident, Tilgang.class);
+            ResponseEntity<Tilgang> response = requestUtenRessurs(restTemplate, uri, personident, Tilgang.class);
             secureLogger.info("Saksbehandler {} forsøker å få tilgang til {} med resultat {}", oidcUtil.getClaim("preferred_username"), personident, response.getBody());
             return response;
         } catch (RestClientException e) {
@@ -187,10 +198,10 @@ public class OppslagTjeneste extends BaseService {
         if (personident == null || personident.isEmpty()) {
             throw new OppslagException("Personident null eller tom");
         }
-        URI uri = URI.create(oppslagServiceUri + "/infotrygd/harBarnAktivKontantstotte");
+        URI uri = URI.create(oppslagServiceUri + "/infotrygd/v1/harBarnAktivKontantstotte");
         logger.info("Henter info om kontantstøtte fra " + oppslagServiceUri);
         try {
-            var response = requestMedPersonIdent(getRestTemplate(), uri, personident, AktivKontantstøtteInfo.class);
+            var response = requestMedPersonIdent(uri, personident, AktivKontantstøtteInfo.class);
             var aktivKontantstøtteInfo = response.getBody();
 
             if (aktivKontantstøtteInfo != null && aktivKontantstøtteInfo.getHarAktivKontantstotte() != null) {
@@ -209,7 +220,6 @@ public class OppslagTjeneste extends BaseService {
         } catch (HttpClientErrorException.NotFound e) {
             secureLogger.info("Personident ikke funnet i infotrygds kontantstøttedatabase. Personident: {}", personident);
             return new AktivKontantstøtteInfo(false);
-
         } catch (RestClientException e) {
             throw new OppslagException("Ukjent feil ved oppslag mot '" + uri + "'.", e, uri, personident);
         }
@@ -222,18 +232,14 @@ public class OppslagTjeneste extends BaseService {
     public PersonhistorikkInfo hentHistorikkFor(String personident, LocalDate fødselsdato) {
         final var fom = fødselsdato;
         final var tom = LocalDate.now();
-        URI uri = URI.create(oppslagServiceUri + "/personopplysning/historikk?fomDato=" + formaterDato(fom) + "&tomDato=" + formaterDato(tom));
+        URI uri = URI.create(oppslagServiceUri + "/personopplysning/v1/historikk?fomDato=" + formaterDato(fom) + "&tomDato=" + formaterDato(tom));
         logger.info("Henter personhistorikkInfo fra " + oppslagServiceUri);
         try {
-            ResponseEntity<PersonhistorikkInfo> response = requestMedPersonIdent(getRestTemplate(), uri, personident, PersonhistorikkInfo.class);
+            ResponseEntity<PersonhistorikkInfo> response = requestMedPersonIdent(uri, personident, PersonhistorikkInfo.class);
             secureLogger.info("Personhistorikk for {}: {}", personident, response.getBody());
             return response.getBody();
-        } catch (HttpStatusCodeException e) {
-            String feilmelding = Optional.ofNullable(e.getResponseHeaders().getFirst("message")).orElse("Ingen feilmelding");
-            logger.warn("Kall mot oppslag feilet ved uthenting av historikk: " + feilmelding);
-            throw new OppslagException(feilmelding);
         } catch (RestClientException e) {
-            throw new OppslagException("Ukjent feil ved oppslag mot '" + uri + "'.", e, uri, personident);
+            throw new OppslagException("Kall mot oppslag feilet ved uthenting av historikk", e, uri, personident);
         }
     }
 
@@ -242,18 +248,14 @@ public class OppslagTjeneste extends BaseService {
         maxAttempts = 3,
         backoff = @Backoff(delay = 5000))
     public Personinfo hentPersoninfoFor(String personIdent) {
-        URI uri = URI.create(oppslagServiceUri + "/personopplysning/info");
+        URI uri = URI.create(oppslagServiceUri + "/personopplysning/v1/info");
         logger.info("Henter personinfo fra " + oppslagServiceUri);
         try {
-            ResponseEntity<Personinfo> response = requestMedPersonIdent(getRestTemplate(), uri, personIdent, Personinfo.class);
+            ResponseEntity<Personinfo> response = requestMedPersonIdent(uri, personIdent, Personinfo.class);
             secureLogger.info("Personinfo for {}: {}", personIdent, response.getBody());
             return response.getBody();
-        } catch (HttpStatusCodeException e) {
-            String feilmelding = Optional.ofNullable(e.getResponseHeaders().getFirst("message")).orElse("Ingen feilmelding");
-            logger.warn("Kall mot oppslag feilet ved uthenting av personinfo: " + feilmelding);
-            throw new OppslagException(feilmelding);
         } catch (RestClientException e) {
-            throw new OppslagException("Ukjent feil ved oppslag mot '" + uri + "'.", e, uri, personIdent);
+            throw new OppslagException("Kall mot oppslag feilet ved uthenting av personinfo", e, uri, personIdent);
         }
     }
 
@@ -262,18 +264,14 @@ public class OppslagTjeneste extends BaseService {
         maxAttempts = 3,
         backoff = @Backoff(delay = 5000))
     public MedlemskapsInfo hentMedlemskapsUnntakFor(AktørId aktørId) {
-        URI uri = URI.create(oppslagServiceUri + "/medlemskap/?id=" + aktørId.getId());
+        URI uri = URI.create(oppslagServiceUri + "/medlemskap/v1/?id=" + aktørId.getId());
         logger.info("Henter medlemskapsUnntak fra " + oppslagServiceUri);
         try {
             ResponseEntity<MedlemskapsInfo> response = request(uri, MedlemskapsInfo.class);
             secureLogger.info("MedlemskapsInfo for {}: {}", aktørId, response.getBody());
             return response.getBody();
-        } catch (HttpStatusCodeException e) {
-            String feilmelding = Optional.ofNullable(e.getResponseHeaders().getFirst("message")).orElse("Ingen feilmelding");
-            logger.warn("Kall mot oppslag feilet ved uthenting av medlemskapsinfo: " + feilmelding);
-            throw new OppslagException(feilmelding);
         } catch (RestClientException e) {
-            throw new OppslagException("Ukjent feil ved oppslag mot '" + uri + "'.", e, uri, aktørId.getId());
+            throw new OppslagException("Kall mot oppslag feilet ved uthenting av medlemskapsinfo", e, uri, aktørId.getId());
         }
     }
 
@@ -282,7 +280,7 @@ public class OppslagTjeneste extends BaseService {
         maxAttempts = 3,
         backoff = @Backoff(delay = 5000))
     public void oppdaterGosysOppgave(String fnr, String journalpostID, String beskrivelse) {
-        URI uri = URI.create(oppslagServiceUri + "/oppgave/oppdater");
+        URI uri = URI.create(oppslagServiceUri + "/oppgave/v1/oppdater");
         logger.info("Sender \"oppdater oppgave\"-request til " + uri);
         Oppgave oppgave = new Oppgave(hentAktørId(fnr).getId(), journalpostID, null, beskrivelse);
         try {
