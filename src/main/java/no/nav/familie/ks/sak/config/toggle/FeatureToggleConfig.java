0f283f6f35f311c297fda8145be02e2cf53b2e25
Geirmund Simonsen <geirmund.simonsen@nav.no>
2019-11-27 11:54:21 +0100

    Lagt til e2e-profil - endret FlywayConfiguration til å kun gjelde for prod / preprod - oppdatert profiler i FeatureToggleConfig - fjernet scope_singleton (da den er default scope)

--- src/main/java/no/nav/familie/ks/sak/config/toggle/FeatureToggleConfig.java
+++ src/main/java/no/nav/familie/ks/sak/config/toggle/FeatureToggleConfig.java
@@ -1,6 +1,7 @@
 package no.nav.familie.ks.sak.config.toggle;
 
 import no.finn.unleash.DefaultUnleash;
+import no.finn.unleash.FakeUnleash;
 import no.finn.unleash.Unleash;
 import no.finn.unleash.strategy.Strategy;
 import no.finn.unleash.util.UnleashConfig;
@@ -11,10 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.Scope;
-
-import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
-
 
 @Configuration
 public class FeatureToggleConfig {
@@ -23,9 +20,8 @@ public class FeatureToggleConfig {
     private static final String UNLEASH_API_URL_PROPERTY_NAME = "${UNLEASH_API_URL}";
     private static final String ENVIRONMENT_NAME = "${ENVIRONMENT_NAME}";
 
-    @Profile("!dev")
+    @Profile("!dev & !e2e")
     @Bean
-    @Scope(SCOPE_SINGLETON)
     @Autowired
     public Unleash unleash(
             @Value(APP_NAME_PROPERTY_NAME) String appName,
@@ -42,13 +38,21 @@ public class FeatureToggleConfig {
         return new DefaultUnleash(config, strategies);
     }
 
-    @Profile("!dev")
+    @Profile("e2e")
+    @Bean
+    @Autowired
+    public Unleash fakeUnleash() {
+        logger.info("oppretter UnleashConfig med en fake Unleash");
+        return new FakeUnleash();
+    }
+
+    @Profile("!dev & !e2e")
     @Bean
     public Strategy isNotProd(@Value(ENVIRONMENT_NAME) String env){
         return new IsNotProdStrategy(env);
     }
 
-    @Profile("!dev")
+    @Profile("!dev & !e2e")
     @Bean
     public Strategy byEnvironment(@Value(ENVIRONMENT_NAME) String env){
         return new ByEnvironmentStrategy(env);
